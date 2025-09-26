d4827df2bd66d1be47bc662c63ba77e31bb6195e
trashydeath797-ops <trashydeath797@gmail.com>
2025-09-27 05:55:11 +0600

    Create main.js

---
+++ Root/js/main.js
@@ -0,0 +1,15 @@
+// main.js
+// Executes after full page load
+window.addEventListener('load', function() {
+    console.log("JS executed after page load");
+
+    // -------------------------------
+    // Monetag Interstitial / Popunder
+    // -------------------------------
+    // Example:
+    // var monetagScript = document.createElement('script');
+    // monetagScript.src = "https://libtl.com/sdk.js?zone=YOUR_ZONE_ID";
+    // document.body.appendChild(monetagScript);
+
+    // Custom JS can be added here
+});
